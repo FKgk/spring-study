c9e0b5b0cefd781a57d193aa5ab7f33ddf73f528
FKgk <rhkd865@gmail.com>
2022-06-19 20:45:46 +0900

    refac: AppConfig role

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -8,15 +8,21 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig  {
     // interface 에만 의존된 구현체에 필요한 구현체 클래스를 할당해준다.
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     public MemberService memberService() {
-        return new MemberServiceImpl(memberRepository);
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository, discountPolicy);
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public  DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 
 }
